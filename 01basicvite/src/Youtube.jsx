4892dbcaa9b03adc76aff8afcaf35e5abfb48813
KUMARSUNNY <ks9107163@gmail.com>
2025-02-19 11:01:16 +0530

    completed with basics

--- 01basicvite/src/Youtube.jsx
+++ 01basicvite/src/Youtube.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Youtube = () => {
   return (
-    <div><h1>Youtube is working</h1></div>
+    <>
+    <h1>Youtube is working</h1>
+    </>
   )
 }
 
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,10 +1,34 @@
 
 import { createRoot } from 'react-dom/client'
-
+ import React from 'react'
+ import Youtube from './Youtube'
+// import App from './App.jsx'
 import App from './App.jsx'
 
+const reactElement = {
+  type: 'a',  // Changed to 'a' since it has href
+  props: {
+      href: 'https://www.youtube.com',
+      target: '_blank',
+  },
+  children: 'Click me to Visit Google'  // Fixed 'chidren' typo
+}
+
+
+// whatever we write in the { brackets react just extract it in the fromat of object and it is the format of the given below if there is an element in the  braces it is added at the end of the object it only takes evaluated statements}
+const areactElement = React.createElement(
+  // the first thing react reads is element type
+  'a',
+  // the second thing react reads is props
+  {
+    href: 'https://www.youtube.com',
+    target: '_blank'
+  },
+  'click to visit youtube'
+)
+
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+   <App />,
+    <Youtube />
   ,
 )
